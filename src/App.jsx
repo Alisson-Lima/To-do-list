2c36ba126d4a197c21c27aab12b30258f5933b61
Alisson Renan <renalima.rl46@gmail.com>
2022-09-12 15:29:47 -0300

    Adding the structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import {Task} from "./components/Task/"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="container">
+      <header>
+        <h1>To-do List</h1>
+        <button className="modeBtn">
+          <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19 12.4746C18.1545 12.7309 17.2575 12.8687 16.3284 12.8687C11.249 12.8687 7.13135 8.75101 7.13135 3.67163C7.13135 2.74246 7.26914 1.84548 7.5254 1C3.74896 2.14461 1 5.65277 1 9.80298C1 14.8824 5.11765 19 10.197 19C14.3472 19 17.8554 16.251 19 12.4746Z" stroke="#252525" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      </header>
+      <form>
+        <input type="text" className="addTask" />
+        <button type="submit" onClick={(e) => e.preventDefault()}>
+          <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.5 10.6H20.5M10.6 20.5V1.5" stroke="white" stroke-width="3" stroke-linecap="round"/></svg>
+        </button>
+      </form>
+      <div className="tasks">
+        <Task />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
---
+++ src/components/Task/index.jsx
@@ -0,0 +1,13 @@
+export function Task(){
+    return(
+        <div className="taks">
+            <label htmlFor="check">
+                <input type="checkbox" name="check" className="checkBox" />
+                Arrumar a cama
+            </label>
+            <button className="deleteTask">
+            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3 4V17C3 18.1046 3.89543 19 5 19H15C16.1046 19 17 18.1046 17 17V4M3 4H6M3 4H1M17 4H19M17 4H14M14 4H6M14 4V3C14 1.89543 13.1046 1 12 1H8C6.89543 1 6 1.89543 6 3V4" stroke="#252525" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>
+            </button>
+        </div>
+    )
+}
